a1e90d1ee2b2f405bbcf09167f98cbac2eb84c04
ryutkura <ryutkura@gmail.com>
2023-09-19 02:21:33 +0900

    メモの追加

--- 05/Chapter5.java
+++ 05/Chapter5.java
@@ -3,15 +3,15 @@ public class Chapter5 {
 
     public static void main(String... args){
         //書物クラスの定義図
-        class Book{
-            //2つのフィールドを持っている
-        String title;
-        String author;
+    class Book{
+    //2つのフィールドを持っている
+    String title;//それぞれ独立した値をセットできる
+    String author;
     }
         
         //1つ目の書物オブジェクト    
-        Book book1 = new Book();
-        book1.title = "Peopleware";
+        Book book1 = new Book();//Bookオブジェクトのtitleフィールドを生成。titleフィールドのフィールド変数
+        book1.title = "Peopleware";//StringオブジェクトのPeoplewareを生成。こいつはフィールドオブジェクト
         
         //2つ目の書物オブジェクト
         Book book2 = new Book();
