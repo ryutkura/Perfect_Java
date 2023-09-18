e17b500f948c13936f4cc11034b984950686f922
ryutkura <ryutkura@gmail.com>
2023-09-19 03:23:42 +0900

    P102まで完了

--- 05/Chapter5.java
+++ 05/Chapter5.java
@@ -3,20 +3,22 @@ public class Chapter5 {
 
     public static void main(String... args){
         //書物クラスの定義図
-    class Book{
-    //2つのフィールドを持っている
-    String title;//それぞれ独立した値をセットできる
-    String author;
+        class Book{
+            //2つのフィールドを持っている
+            String title;//それぞれ独立した値をセットできる
+            String author;
 
-    //メソッドの概要
-    void printTitle(){
-        System.out.println(this.title);//thisは省略可能
-    }
-    }
+            //メソッド StringBuilderのlengthとかappendとか
+            void printTitle(){
+                //メソッドにはオブジェクトに対する処理が書かれている
+                System.out.println(this.title);//thisは省略可能
+            }
+        }
         
         //1つ目の書物オブジェクト    
         Book book1 = new Book();//Bookオブジェクトのtitleフィールドを生成。titleフィールドのフィールド変数
         book1.title = "Peopleware";//StringオブジェクトのPeoplewareを生成。こいつはフィールドオブジェクト
+        book1.printTitle();//オブジェクトに処理を依頼したりオブジェクトの状態を知るために呼び出す
         
         //2つ目の書物オブジェクト
         Book book2 = new Book();
