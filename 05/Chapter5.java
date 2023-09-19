8d335e7fe171c6eb6c81769e874831a04994705c
ryutkura <ryutkura@gmail.com>
2023-09-19 23:52:50 +0900

    今日の勉強ここまで

--- 05/Chapter5.java
+++ 05/Chapter5.java
@@ -19,9 +19,12 @@ public class Chapter5 {
         Book book1 = new Book();//Bookオブジェクトのtitleフィールドを生成。titleフィールドのフィールド変数
         book1.title = "Peopleware";//StringオブジェクトのPeoplewareを生成。こいつはフィールドオブジェクト
         book1.printTitle();//オブジェクトに処理を依頼したりオブジェクトの状態を知るために呼び出す
+        //レジィの術式みたく↑こいつらに実体がある。概念的に雛形がクラスにあるのは術式と同じ、術式発動したらレシートは実体になる
         
         //2つ目の書物オブジェクト
         Book book2 = new Book();
         book2.title = "Showstopper!";
+
+        //
     }
 }
