541d269f9bd255991afe72b1525fdeec530344d0
ryutkura <ryutkura@gmail.com>
2023-09-19 16:53:46 +0900

    研究室でmainブランチの内容を変更しました

--- 05/Chapter5.java
+++ 05/Chapter5.java
@@ -1,13 +1,13 @@
 public class Chapter5 {
 
 
-    public static void main(String... args){
+    public static void main(String[] args){
         //書物クラスの定義図
-    class Book{
-    //2つのフィールドを持っている
-    String title;//それぞれ独立した値をセットできる
-    String author;
-    }
+        class Book{
+        //2つのフィールドを持っている
+        String title;//それぞれ独立した値をセットできる
+        String author;
+        }
         
         //1つ目の書物オブジェクト    
         Book book1 = new Book();//Bookオブジェクトのtitleフィールドを生成。titleフィールドのフィールド変数
@@ -16,5 +16,8 @@ public class Chapter5 {
         //2つ目の書物オブジェクト
         Book book2 = new Book();
         book2.title = "Showstopper!";
+
+        System.out.println("これは研究室で書いたコードです。\nこれが成功したらわざわざPCを持ち歩かなくても\nファイル管理ができるので研究が進みます");
+        System.out.println("現在mainブランチにて作業中です。");
     }
 }
