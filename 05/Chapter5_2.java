8764041675d2858e2d733d387b91bd81c6864b1b
ryutkura <ryutkura@gmail.com>
2023-09-22 17:38:56 +0900

    メソッド等のスコープが確認できました

--- 05/Chapter5_2.java
+++ 05/Chapter5_2.java
@@ -9,17 +9,33 @@ public class Chapter5_2 {
 
             //メソッド内でsを使用可能
             void method(){
-                System.out.println("これはmethodの"+s);
+                System.out.println("これはmethodの"+s);//このmethod全部fooが表示された
                 System.out.println("これはmethodの"+s2);
+                System.out.println("これはmethodのthis."+this.s);
+            }
+            
+            void method2(){
+                System.out.println("これはmethod2の"+s);//このmethod全部fooが表示された
+                System.out.println("これはmethod2のthis."+this.s);
+            }
+            
+            My(String s){
+                System.out.println("これはMy(String)の"+ s);/*ここで一発キヨシが表示された*/
+                System.out.println("これはMy(Stirng)のthis."+this.s);/*この行ではfooが表示された */
             }
 
             My(){
-                System.out.println("これはコンストラクタ内での"+s);
+                String s = "bar";
+                System.out.println("これはコンストラクタ内での"+s);/*この行ではbarが表示された*/
+                System.out.println("これはコンストラクタ内でのthis."+this.s);//この行ではfooが表示された
             }
         }
 
-        My my = new My();
+        My my = new My("ここで一発キヨシ！");
         my.method();
+        
+        My my2 = new My();
+        my2.method2();
         
 
 
